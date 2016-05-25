116a5cd5ac669ff36d018099beea61638abea343
Daniel Fiore <taptapdan@gmail.com>
2016-05-24 18:36:29 -0700

    Update exercise001.js

--- problems/exercise001.js
+++ problems/exercise001.js
@@ -1,3 +1,9 @@
+/*
+  imaginaryFunction
+  - Create a function named imaginaryFunction
+  - imaginaryFunction should return the string 'foon'
+*/
+
 var imaginaryFunction = function() {
-  return 'foon';
+  
 };
